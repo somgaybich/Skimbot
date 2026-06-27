805083c9d22ce285f9e3247c795a10f8dc2e52d0
somgaybich <imbow568@gmail.com>
2026-06-27 02:03:35 -0400

    Shell fix

--- bin/skimbot.sh
+++ bin/skimbot.sh
@@ -1,2 +1,3 @@
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-"$SCRIPT_DIR/.venv/bin/python" "$SCRIPT_DIR/core.py" "$@"
+PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+"$PROJECT_DIR/.venv/bin/python" "$PROJECT_DIR/core.py" "$@"
